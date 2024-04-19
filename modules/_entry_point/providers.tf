e8a3d823e7c17b3d8a2dc5d557caac5369e88f02
James Mullan <j.p.mullan@outlook.com>
2024-04-19 12:06:53 +0100

    added framework module

---
+++ modules/_entry_point/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.89.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  # Configuration options
+}
